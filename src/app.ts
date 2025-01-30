efe8d2d978b09a0358af2d948b5e78b3f96161a6
rdagnaud <dagnaud.r@gmail.com>
2025-01-30 15:17:31 +0100

    Setup of typescript project

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from 'cors'
+import express from 'express'
+import { config } from '~/config'
+import { ExceptionsHandler } from '~/middlewares/exceptions.handler'
+import { UnknownRoutesHandler } from '~/middlewares/unknownRoutes.handler'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+/**
+ * Gestion des erreurs
+ */
+app.all('*', UnknownRoutesHandler)
+app.use(ExceptionsHandler)
+
+app.listen(config.API_PORT, () => console.log(`Server running on port ${config.API_PORT}`))
---
+++ src/utils/exceptions.ts
@@ -0,0 +1,20 @@
+import { ApiException } from '~~/types/exceptions'
+
+class Exception implements ApiException {
+    constructor(readonly error: any, readonly status: number) {}
+}
+
+export class NotFoundException extends Exception {
+    constructor(error: any) {
+      super(error, 404)
+    }
+  }
+  
+  /**
+   * Création d'une 400
+   */
+  export class BadRequestException extends Exception {
+    constructor(error: any) {
+      super(error, 400)
+    }
+  }
